f6cfae85456e67520af601e063b8e6fa7429effb
Xalandra <xalandra@windowslive.com>
2017-03-14 18:04:23 +0100

    first commit

---
+++ app/components/App.js
@@ -0,0 +1,27 @@
+var React = require('react');
+
+var ToDoList = React.createClass({
+   getInitialState: function() {
+       return {
+           tasks: []
+       };
+   },
+
+    addTask: function(event) {
+
+    },
+
+    render: function(){
+        return (
+            <div>
+                <h1>To-Do-List</h1>
+                <form onSubmit={this.addTask}>
+                    Task: <input placeholder="Enter task" />
+                    <button type="submit">Add</button>
+                </form>
+            </div>
+        );
+    }
+});
+
+module.exports = ToDoList;
