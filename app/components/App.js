2e0da4d1559dc446611abff1e4dc448b88b967a3
Xalandra <xalandra@windowslive.com>
2017-03-15 15:52:40 +0100

    Created TodoItems component Added functionalty to TodoList component

--- app/components/App.js
+++ app/components/App.js
@@ -1,24 +1,61 @@
 var React = require('react');
 
+var TodoItems = React.createClass({
+    render: function() {
+        var todoEntries = this.props.entries;
+
+        function createTasks(item) {
+            return <li key={item.key}>{item.text}</li>
+        }
+
+        var listItems = todoEntries.map(createTasks);
+
+        return (
+            <ul>
+                {listItems}
+            </ul>
+        );
+    }
+});
+
 var ToDoList = React.createClass({
    getInitialState: function() {
        return {
-           tasks: []
+           items: []
        };
    },
 
-    addTask: function(event) {
-        this.setState.tasks;
+    addItem: function(event) {
+        var itemArray = this.state.items;
+
+        // add submitted text
+        itemArray.push(
+            {
+                text: this._inputElement.value,
+                key: Date.now()  // current time as unique id
+            }
+        );
+
+        this.setState({
+            items: itemArray
+        });
+
+        // empty text input field
+        this._inputElement.value = "";
+
+        // prevent POST behavior
+        event.preventDefault();
     },
 
     render: function(){
         return (
             <div>
                 <h1>To-Do-List</h1>
-                <form onSubmit={this.addTask}>
-                    Task: <input placeholder="Enter task" />
+                <form onSubmit={this.addItem}>
+                    Task: <input ref={(a) => this._inputElement = a} placeholder="Enter task" />
                     <button type="submit">Add</button>
                 </form>
+                <TodoItems entries={this.state.items} />
             </div>
         );
     }
